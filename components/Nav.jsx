2337075637d0b1fc521755d63de5ef1fbf0a4fbb
Obi ifeanyi <obiifeanyi.fi@gmail.com>
2024-09-16 16:03:17 +0100

    Corrected the desktop create post link

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -38,7 +38,7 @@ const Nav = () => {
       <div className="sm:flex hidden">
         {session?.user ? (
           <div className="flex gap-3 md:gap-5">
-            <Link className="black_btn" href="/create-posts">
+            <Link className="black_btn" href="/create-propmts">
               Create Post
             </Link>
 
